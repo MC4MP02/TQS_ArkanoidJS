3119dbb73f8f44c588b1fbb561127ce5c57217b1
MC4MP02 <mcampocabrera2013@gmail.com>
2024-10-06 16:57:39 +0200

    First structure created, created MVC body with logical clases and test clases

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+// Punto de entrada
+
+const canvas = document.getElementById("game") as HTMLCanvasElement;
+const ctx = canvas.getContext("2d");
+
+const canvasWidth = 448;
+const canvasHeight = 400;
+
+canvas.width = canvasWidth;
+canvas.height = canvasHeight;
+
+//Inicializar la vista del juego
+
+// Empezar el juego en el controlador con la vista
+
+// Manejar la entrada del usuario y usar controlador para gestionar

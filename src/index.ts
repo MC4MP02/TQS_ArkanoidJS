084f79242319d646561f74817e23d4cade69cb1b
MC4MP02 <mcampocabrera2013@gmail.com>
2024-10-07 20:59:26 +0200

    Added Vite to compile scripts

--- src/index.ts
+++ src/index.ts
@@ -6,15 +6,11 @@ import { GameView } from "./view/GameView";
 const canvas = document.getElementById("game") as HTMLCanvasElement;
 const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
 
-const canvasWidth = 448;
-const canvasHeight = 400;
-
-canvas.width = canvasWidth;
-canvas.height = canvasHeight;
-
 const view = new GameView(canvas, ctx);
 
 const gameController = new GameController(view);
+
+view.loadCanvas();
 
 window.onload = () => {
   gameController.startGame();
--- src/view/GameView.ts
+++ src/view/GameView.ts
@@ -2,8 +2,16 @@ export class GameView {
   private canvas: HTMLCanvasElement;
   private ctx: CanvasRenderingContext2D;
 
+  private canvasWidth: number = 448;
+  private canvasHeight: number = 400;
+
   constructor(canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D) {
     this.canvas = canvas;
     this.ctx = ctx;
+  }
+
+  loadCanvas() {
+    this.canvas.width = this.canvasWidth;
+    this.canvas.height = this.canvasHeight;
   }
 }

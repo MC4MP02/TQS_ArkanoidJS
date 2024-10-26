b3650a453c6275ab325b38614f851fc86ca779d9
MC4MP02 <mcampocabrera2013@gmail.com>
2024-10-26 14:32:31 +0200

    Added first implementation of GameView.drawMap() also added the sprite in the index.html so it can be found

--- src/view/GameView.ts
+++ src/view/GameView.ts
@@ -10,21 +10,23 @@ export class GameView {
     "#sprite"
   ) as HTMLImageElement;
 
+  private bricksSprite: HTMLImageElement = document.querySelector(
+    "#bricks"
+  ) as HTMLImageElement;
+
   private canvasWidth: number = 448;
   private canvasHeight: number = 400;
+
+  private BRICK_STATUS: { ALIVE: number; DEAD: number } = {
+    ALIVE: 1,
+    DEAD: 0,
+  };
 
   constructor(canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D) {
     this.canvas = canvas;
     this.ctx = ctx;
     this.sprite.src = "./assets/sprite.png";
-
-    /*this.sprite.onload = () => {
-      this.loadCanvas();
-
-      this.drawBall(new Ball(50, 50, 10, 2, 2));
-      const paddle = new Paddle(50, 10, (448 - 50) / 2, 400 - 10 - 20);
-      this.drawPaddle(paddle);
-    };*/
+    /* this.bricksSprite.src = "./assets/bricks.png"; */
   }
 
   loadCanvas() {
@@ -56,7 +58,30 @@ export class GameView {
     );
   }
 
-  private drawMap(map: Map) {}
+  private drawMap(map: Map) {
+    const bricks = map.getBricks();
+    const brickWidth = map.getBrickWidth();
+    const brickHeigth = map.getBrickHeigth();
+
+    for (let c = 0; c < map.getBrickColumnCount(); c++) {
+      for (let r = 0; r < map.getBrickRowCount(); r++) {
+        const currentBrick = bricks[c][r];
+        if (currentBrick.status === this.BRICK_STATUS.DEAD) continue;
+
+        this.ctx.drawImage(
+          this.bricksSprite,
+          currentBrick.color * 32,
+          0,
+          brickWidth,
+          brickHeigth,
+          currentBrick.BrickX,
+          currentBrick.BrickY,
+          brickWidth,
+          brickHeigth
+        );
+      }
+    }
+  }
 
   clearCanvas() {
     this.ctx.clearRect(0, 0, this.canvasWidth, this.canvasHeight);

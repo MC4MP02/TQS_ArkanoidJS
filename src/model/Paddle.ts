6a23e4745ea1f905111ec9139cc98ac89a6a9aae
Pol <polcaparrosescudero@gmail.com>
2024-11-11 23:42:20 +0100

    Paddle Constructor v3

--- src/model/Paddle.ts
+++ src/model/Paddle.ts
@@ -1,10 +1,14 @@
 export class Paddle {
   public paddleWidth: number;
   public paddleHeight: number;
+  public paddleX: number;
+  public paddleY: number;
 
 
-  constructor(paddleWidth: number, paddleHeight: number) {
+  constructor(paddleWidth: number, paddleHeight: number, paddleX: number, paddleY: number) {
     this.paddleWidth = paddleWidth;
     this.paddleHeight = paddleHeight;
+    this.paddleX = paddleX;
+    this.paddleY = paddleY;
   }
 }

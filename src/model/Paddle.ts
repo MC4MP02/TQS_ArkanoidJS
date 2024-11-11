21f9095ac2fa521793cdd468bf49295d65cecc5a
Pol <polcaparrosescudero@gmail.com>
2024-11-11 23:37:22 +0100

    Paddle Constructor implementation v2

--- src/model/Paddle.ts
+++ src/model/Paddle.ts
@@ -1,7 +1,10 @@
 export class Paddle {
   public paddleWidth: number;
+  public paddleHeight: number;
 
-  constructor(paddleWidth: number) {
+
+  constructor(paddleWidth: number, paddleHeight: number) {
     this.paddleWidth = paddleWidth;
+    this.paddleHeight = paddleHeight;
   }
 }
--- test/model/Paddle.test.ts
+++ test/model/Paddle.test.ts
@@ -2,7 +2,7 @@ import { Paddle } from "../../src/model/Paddle";
 
 describe("Paddle", () => {
   it("debería asignar el ancho de la pala (paddleWidth) correctamente", () => {
-    const paddle = new Paddle(100);
+    const paddle = new Paddle(100,20);
     expect(paddle.paddleWidth).toBe(100);
   });
   it("debería asignar la altura de la pala (paddleHeight) correctamente", () => {

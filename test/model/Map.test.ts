7712c3083e99efedb2b1fc7c3e9056aaac2caaf3
MC4MP02 <mcampocabrera2013@gmail.com>
2024-11-21 13:05:31 +0100

    created tests for Map getters

--- test/model/Map.test.ts
+++ test/model/Map.test.ts
@@ -1,5 +1,29 @@
+import { Map } from "../../src/model/Map";
+
 describe("Map", () => {
-  it("debería tener un test", () => {
-    expect(true).toBe(true);
+  let map: Map;
+
+  beforeEach(() => {
+    map = new Map();
+  });
+
+  it("debería devolver el array de bricks con getBricks", () => {
+    expect(map.getBricks()).toEqual([]); // Valor inicial por defecto
+  });
+
+  it("debería devolver el número de columnas de ladrillos con getBrickColumnCount", () => {
+    expect(map.getBrickColumnCount()).toBe(0); // Valor inicial por defecto
+  });
+
+  it("debería devolver el número de filas de ladrillos con getBrickRowCount", () => {
+    expect(map.getBrickRowCount()).toBe(0); // Valor inicial por defecto
+  });
+
+  it("debería devolver el ancho de los ladrillos con getBrickWidth", () => {
+    expect(map.getBrickWidth()).toBe(0); // Valor inicial por defecto
+  });
+
+  it("debería devolver la altura de los ladrillos con getBrickHeigth", () => {
+    expect(map.getBrickHeigth()).toBe(0); // Valor inicial por defecto
   });
 });
